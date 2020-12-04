473787a46e52d32fe8a973c571355689160ce435
Ali Raza <alirazamunir401@gmail.com>
2020-12-04 22:31:34 +0500

    day 4_2

---
+++ src/pages/CreateWorker.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import bannerImg from "./../assets/img/body-bg.jpg";
+
+
+function CreateWorker() {  
+
+  return (
+    <div className="create-worker-page">
+        <div className="back_img vh-100 overflow-y-scroll" style={{ backgroundImage: `url(${bannerImg})` }} >
+            <div className="row w-100 pl-10 pt-4">
+                <div className="col-md-6 bg-white py-4 px-3 rounded">
+                    <form action="" className="">
+                        <h4>Check video</h4>
+                            <div className="form-video mb-4">
+                                <iframe width="100%" height="350" src="https://www.youtube.com/embed/35lXWvCuM8o" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                        <h4 className="mb-3">Create worker Form</h4>
+                        <input type="text" placeholder="Enter Name" className="form-control rounded-0 mb-2"/>
+                        <div className="row">
+                            <div className="col-6 pr-1">
+                                <input type="text" placeholder="Enter Name" className="form-control rounded-0  mb-2"/>
+                            </div>
+                            <div className="col-6 pl-1">
+                                <input type="text" placeholder="Enter Name" className="form-control rounded-0  mb-2"/>
+                            </div>
+                        </div>
+                        <input type="text" placeholder="Enter Name" className="form-control rounded-0 mb-2"/>
+                        <input type="text" placeholder="Enter Name" className="form-control rounded-0 mb-2"/>
+                        <input type="text" placeholder="Enter Name" className="form-control rounded-0 mb-3"/>
+
+                        <a href="" className="btn btn-block rounded-0 bg-pri text-white">Create Worker</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default CreateWorker;
---
+++ src/pages/Services.jsx
@@ -0,0 +1,292 @@
+import React from "react";
+
+import productImg from "./../assets/img/product.jpg";
+import { Navbar, Nav } from "react-bootstrap";
+
+function Services() {
+  return (
+    <div className="services-page container">
+      <Navbar bg="light" variant="light" className="bg-transparent">
+        <Navbar.Brand href="#home">LOGO</Navbar.Brand>
+        <Nav className="ml-auto">
+          <Nav.Link href="#home">Location</Nav.Link>
+          <Nav.Link href="#features">Service</Nav.Link>
+          <Nav.Link href="#pricing">Login</Nav.Link>
+          <Nav.Link href="#new">Signup</Nav.Link>
+          <Nav.Link href="#pricing" className="btn header-btn rounded-0 rounded px-3">
+            Become a Worker
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+
+      <div className="sec-services pt-5">
+        <div className="container">
+          <h4 className=" mb-2">Tranding services</h4>
+          <div className="sec-2">
+            <div className="row">
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow ">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="sec-services pt-5">
+        <div className="container">
+          <h4 className=" mb-2">Popular n your area</h4>
+          <div className="sec-2">
+            <div className="row">
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow ">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="sec pt-5">
+        <div className="container">
+          <h4 className=" mb-2">Best budget</h4>
+          <div className="sec-2">
+            <div className="row">
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow ">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3 mb-4">
+                <div className="mini-card rounded shadow">
+                  <img src={productImg} className="img img-fluid" />
+                  <div className="text text-center">
+                    <p className="mb-0 mt-2">Assamble desk</p>
+                    <p className="text-secondary"> Avg -price Rs500-Rs2000 </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Services;
